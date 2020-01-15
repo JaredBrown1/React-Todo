7e50a6aedaa9cd383bee2fb5662385cf72889bf4
Jared Brown <jaredbrown1@hotmail.com>
2020-01-15 11:29:47 -0800

    created todo Data

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const todoData = [
+  {
+    task: "Sweep floors",
+    id: 1,
+    completed: false
+  },
+  {
+    task: "Bake Cookies",
+    id: 2,
+    completed: false
+  },
+  {
+    task: "Clean Bathroom",
+    id: 3,
+    completed: false
+  }
+];
+
+export default todoData;
