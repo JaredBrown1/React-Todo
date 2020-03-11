b6605a2fabe75b8ada437eda393a8dc7e7cc2c5f
Jared Brown <jaredbrown1@hotmail.com>
2020-03-11 11:27:51 -0700

    restarting for flex

--- src/App.js
+++ src/App.js
@@ -1,87 +1,13 @@
-import React from "react";
-
-import TodoForm from "./components/TodoComponents/TodoForm";
-import Tasks from "./components/TodoComponents/TodoList";
-
-const tasks = [
-  {
-    task: "Sweep floors",
-    id: 1,
-    completed: false
-  },
-  {
-    task: "Bake Cookies",
-    id: 2,
-    completed: false
-  },
-  {
-    task: "Clean Bathroom",
-    id: 3,
-    completed: false
-  }
-];
+import React from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-
-  constructor() {
-    super();
-    this.state = {
-      todoList: tasks
-    };
-  }
-
-  addNewTask = newTaskName => {
-    const newState = {
-      ...this.state,
-      todoList: [
-        ...this.state.todoList,
-        { name: newTaskName, completed: false, id: Date.now() }
-      ]
-    };
-    this.setState(newState);
-  };
-
-  toggleComplete = id => {
-    const newState = {
-      ...this.state,
-      todoList: this.state.todoList.map(task => {
-        if (task.id === id) {
-          return {
-            ...task,
-            completed: !task.completed
-          };
-        }
-        return task;
-      })
-    };
-    this.setState(newState);
-  };
-
-  clearCompleted = () => {
-    const newState = {
-      ...this.state,
-      todoList: this.state.todoList.filter(task => {
-        return !task.completed;
-      })
-    };
-    this.setState(newState);
-  };
-
   render() {
     return (
-      <div className="App">
-        <div>
-          <h2>Welcome to your Todo App!</h2>
-          <TodoForm addNewTask={this.addNewTask} />
-        </div>
-        <Tasks
-          tasks={this.state.todoList}
-          toggleComplete={this.toggleComplete}
-          clearCompleted={this.clearCompleted}
-        />
+      <div>
+        <h2>Welcome to your Todo App!</h2>
       </div>
     );
   }
